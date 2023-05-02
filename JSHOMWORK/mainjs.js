3b23537b6f6a8ef9e7df2f70faa06b585b72f65a
BelkaOlena <elenasobyaninabelka@gmail.com>
2023-05-02 18:08:04 +0300

    js1hw

---
+++ JSHOMWORK/mainjs.js
@@ -0,0 +1,46 @@
+
+let a1 = 'hello'
+let a2 = 'owu'
+let a3 = 'com'
+let a4 = 'ua'
+let a5 = 1
+let a6 = 10
+let a7 = -999
+let a8 = 3.14
+let a9 = 2.7
+let a10 = 16
+let a11 = true
+let a12 = false
+console.log(a1)
+console.log(a2)
+console.log(a3)
+console.log(a4)
+console.log(a5)
+console.log(a6)
+console.log(a7)
+console.log(a8)
+console.log(a9)
+console.log(a10)
+console.log(a11)
+console.log(a12)
+
+let firstName = 'Olena'
+let middleName = 'Vasilivna'
+let lastName = 'Sobianina'
+// console.log(firstName, middleName, lastName)
+let user = `${firstName} ${middleName} ${lastName}`
+console.log(user)
+
+let a = 100
+let b = '100'
+let c = true
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+let name = prompt()
+let middle = prompt()
+let age = prompt()
+console.log(name)
+console.log(middle)
+console.log(age)
